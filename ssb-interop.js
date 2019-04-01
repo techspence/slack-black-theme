a99aa18289b83279216316edd2e174a3bb8e3ecb
Spencer Alessi <alessisb@gmail.com>
2019-04-01 15:15:19 -0400

    Changed cssPath to my repo

--- ssb-interop.js
+++ ssb-interop.js
@@ -106,7 +106,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Then get its webviews
     let webviews = document.querySelectorAll(".TeamView webview");
     // Fetch our CSS in parallel ahead of time
-    const cssPath = 'https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css';
+    const cssPath = 'https://raw.githubusercontent.com/techspence/slack-black-theme/master/black.css';
     const localCssPath = 'sib.css';
     let cssPromise = fetch(cssPath).then(response => response.text());
 
